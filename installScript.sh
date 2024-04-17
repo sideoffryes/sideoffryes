f3455be4028ff677e831b778669e21a1e033b7c2
Henry Frye <hcfrye1163@gmail.com>
2024-04-17 15:42:58 -0400

    added install script

---
+++ installScript.sh
@@ -0,0 +1,23 @@
+# fetch and install all updates
+echo "[*] sudo apt update && sudo apt upgrade"
+sudo apt update && sudo apt upgrade
+
+# python, pip, and pwntools
+echo "[*] apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential"
+sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+echo "[*] python3 -m pip install --upgrade pip"
+python3 -m pip install --upgrade pip
+echo "[*] python3 -m pip install --upgrade pwntools"
+python3 -m pip install --upgrade pwntools
+
+# gef
+echo "[*] bash -c '$(wget --no-check-certificate https://gef.blah.cat/sh -O -)'"
+bash -c "$(wget --no-check-certificate https://gef.blah.cat/sh -O -)"
+
+# eog
+echo "[*] sudo apt install eog"
+sudo apt install eog
+
+# net-tools
+echo "[*] sudo apt install net-tools"
+sudo apt install net-tools
