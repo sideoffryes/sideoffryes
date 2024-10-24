f3d3dc37ff403487471d2587b322108f9abaa9bb
Henry Frye <hcfrye1163@gmail.com>
2024-10-23 21:10:39 -0400

    added a download all button to download all the photos displayed

--- web/autoMidsServer.py
+++ web/autoMidsServer.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_file
 from autoMidsBackend import get_schedule, get_photos
+import io
+import zipfile
+import os
 
 app = Flask(__name__)
 
@@ -19,6 +22,20 @@ def photos():
     year = request.form['year']
     photos = get_photos(company, year)
     return render_template('index.html', photos=photos[1], path=photos[0])
+
+@app.route('/download_all/<path:dir_name>')
+def download_all(dir_name):
+    zip_buffer = io.BytesIO()
+
+    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        image_folder = os.path.join(app.static_folder, dir_name)
+        for filename in os.listdir(image_folder):
+            file_path = os.path.join(image_folder, filename)
+            if os.path.isfile(file_path):
+                zip_file.write(file_path, os.path.basename(file_path))
+
+    zip_buffer.seek(0)
+    return send_file(zip_buffer, mimetype="application/zip", as_attachment=True, download_name="images.zip")
 
 @app.route('/freeperiods')
 def freeperiods():
