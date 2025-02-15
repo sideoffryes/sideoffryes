a6b1920e20eeec52d343b83710fca6b0cf2abd92
Henry Frye <hcfrye1163@gmail.com>
2025-02-15 17:56:24 -0500

    updates

--- installScript.sh
+++ installScript.sh
@@ -1,16 +1,10 @@
 # fetch and install all updates
-echo "[*] sudo apt update && sudo apt upgrade"
-sudo apt update && sudo apt upgrade
+echo "[*] sudo apt update && sudo apt upgrade -y"
+sudo apt update && sudo apt upgrade -y
 
 # python, pip, pwntools, and ROPgadget
-echo "[*] apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential"
-sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
-echo "[*] python3 -m pip install --upgrade pip"
-python3 -m pip install --upgrade pip
-echo "[*] python3 -m pip install --upgrade pwntools"
-python3 -m pip install --upgrade pwntools
-echo "[*] sudo -H python3 -m pip install ROPgadget"
-sudo -H python3 -m pip install ROPgadget
+echo "[*] sudo apt install -y python3 python3-pip git build-essential"
+sudo sudo apt install -y python3 python3-pip git build-essential
 
 # gdb
 echo "[*] sudo apt install gdb"
@@ -20,10 +14,6 @@ sudo apt install gdb
 echo "[*] bash -c '$(wget --no-check-certificate https://gef.blah.cat/sh -O -)'"
 bash -c "$(wget --no-check-certificate https://gef.blah.cat/sh -O -)"
 
-# eog
-echo "[*] sudo apt install eog"
-sudo apt install eog
-
 # net-tools
 echo "[*] sudo apt install net-tools"
 sudo apt install net-tools
@@ -31,3 +21,21 @@ sudo apt install net-tools
 # unzip
 echo "[*] sudo apt install unzip && sudo apt install zip"
 sudo apt install unzip && sudo apt install zip
+
+# htop 
+echo "[*] sudo apt install htop"
+sudo apt install htop
+
+# miniconda
+echo "mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+conda init --all"
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+conda init --all
